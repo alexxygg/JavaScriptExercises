0ab7f6cc8cba96d3998ea30694222f08b7ab423f
alexxygg <alexxygg@gmail.com>
2022-10-19 22:33:23 -0700

    Add files via upload

---
+++ forLoop/forLoop.js
@@ -0,0 +1,55 @@
+// // console.log(1);
+// // console.log(2);
+// // console.log(3);
+// // console.log(4);
+// // console.log(5);
+// // console.log(6);
+// // console.log(7);
+// // console.log(8);
+// // console.log(9);
+// // console.log(10);
+
+// // for (let i = 1; i <= 10; i++) {
+// //   console.log(i);
+// // }
+
+// // for (let i = 25; i <= 50; i++) {
+// //   console.log(i);
+// // }
+
+// //Prints 0 to 10 (0,1,2,3,4,5,6,7,8,9,10)
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// //Prints even numbers, including 0 (0,2,4,6,8,10)
+// for (let i = 0; i <= 10; i += 2) {
+//   console.log(i);
+// }
+
+// //Prints odd numbers, including 1 (1,3,5,7,9)
+// for (let i = 1; i <= 10; i += 2) {
+//   console.log(i);
+// }
+
+// //Prints numbers in intervals of 10 going in reverse,
+// //we start with 100 and end in 0.
+// //including 100 (100,90,80,70,60,50,40,30,20,10,0)
+// for (let i = 100; i >= 0; i -= 10) {
+//   console.log(i);
+// }
+
+// //Multiplying (1,10,100,1000)
+// for (let i = 1; i <= 1000; i *= 10) {
+//   console.log(i);
+// }
+
+// //Dividing (1000,100,10,1)
+// for (let i = 1000; i >= 1; i /= 10) {
+//   console.log(i);
+// }
+
+// //Multiples of 5 in reverse with substraction, including 0 (25,20,15,10,5,0)
+// for (let i = 25; i >= 0; i -= 5) {
+//   console.log(i);
+// }
